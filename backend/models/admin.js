631c6af4b1c23c9a3ec8eceea012077b45ef837a
Savani-Bondre <98508261+Savani-Bondre@users.noreply.github.com>
2024-06-15 17:02:55 +0530

    Delete backend/models directory

--- backend/models/admin.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const AdminSchema = new mongoose.Schema({
-    username: { type: String, unique: true },
-    password: String,
-},{timestamps:true})
-
-
-export const Admin = mongoose.model("Admins",AdminSchema);
