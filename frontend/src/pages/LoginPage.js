1c976e4cd89d5d82745edc27813e5ab54c49a33d
SnehaPrasad23 <snehaprasad.in@gmail.com>
2024-06-15 23:24:23 +0530

    deleted authContext

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/LoginPage.css';
 
